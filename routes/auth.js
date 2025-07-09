eda2b7f6c3c583262b6011eacc3c851aefc64cfa
Diksha Katiyar <154581716+dikshakatiyar@users.noreply.github.com>
2025-07-09 09:46:54 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { check } = require('express-validator');
+const validate = require('../middleware/validate');
 
 router.post('/register', [
   check('email').isEmail().normalizeEmail(),
